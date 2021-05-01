99d4ad9ace22ade37fa0d0c095e929b88b6908b7
Luis Arellano <luiseduardo08054@gmail.com>
2021-05-01 11:11:49 -0500

    LAR-FEAT: Creacion de objetos y recuperacion de informacion

--- picoPlacaFunctions/functions.js
+++ picoPlacaFunctions/functions.js
@@ -1,1 +1,23 @@
 //ARCHIVO DE FUNCIONES
+function getInformation(){
+    function Vehicle(plate){
+        this.plate=plate;
+    }
+
+    function PicoYPlaca(date,hour){
+        this.date=date;
+        this.hour=hour;
+    }
+
+    var plateInput= document.getElementById("plateInp").value;
+    var dateInput= document.getElementById("dateInp").value;
+    var hourInput= document.getElementById("hourInp").value;
+    console.log(plateInput + " " + dateInput +" " + hourInput)
+
+    var newVehicle= Vehicle(plateInput);
+    var newMovDate = PicoYPlaca(dateInput,hourInput);
+
+
+
+}
+
